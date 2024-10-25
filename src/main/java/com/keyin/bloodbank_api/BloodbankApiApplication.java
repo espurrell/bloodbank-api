bc66249f64653b7154663c67435f9242d41071e7
espurrell <150044207+espurrell@users.noreply.github.com>
2024-10-25 20:23:37 -0230

    initial commit

---
+++ src/main/java/com/keyin/bloodbank_api/BloodbankApiApplication.java
@@ -0,0 +1,13 @@
+package com.keyin.bloodbank_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodbankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodbankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keyin/bloodbank_api/BloodbankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyin.bloodbank_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodbankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
